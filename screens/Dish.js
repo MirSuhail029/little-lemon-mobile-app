c8cf9afcac04196a24573ca29d206bda8e05fcbb
suhail <mirsuhail029@gmail.com>
2023-07-21 17:27:22 +0530

    made changes to the Dish component

--- screens/Dish.js
+++ screens/Dish.js
@@ -10,10 +10,12 @@ import Colors from "../constants/Colors";
 import BurgerMenuModal from "../components/BurgerMenuModal";
 import Navigation from "../components/Navigation";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 const Dish = function (props) {
   const { dishInfo } = props.route.params;
   const [image, dishName, description, price, ...rest] = dishInfo;
+  const [quantity, setQuantity] = useState(1);
   return (
     <>
       <BurgerMenuModal
@@ -27,32 +29,46 @@ const Dish = function (props) {
       </SafeAreaView>
       <ScrollView>
         <View style={styles.rootContainer}>
+          <View style={styles.imageContainer}>
+            <Image source={image} style={styles.image} />
+          </View>
           <View style={styles.contentContainer}>
-            <View style={styles.imageContainer}>
-              <Image source={image} style={styles.image} />
-            </View>
             <Text style={styles.dishName}>{dishName}</Text>
-
             <Text style={styles.description}>{description}</Text>
-            <Text style={styles.price}>{price}</Text>
-            <View style={styles.controls}>
-              <View style={styles.control}>
-                <Pressable android_ripple={{ color: Colors.rippleDark }}>
-                  <Text style={styles.controlText}>Buy</Text>
-                </Pressable>
-              </View>
-              <View>
-                <Pressable
-                  style={styles.control}
-                  android_ripple={{ color: Colors.rippleDark }}
-                  onPress={() => {
-                    props.navigation.navigate("Home");
-                  }}
-                >
-                  <Text style={styles.controlText}>Back</Text>
-                </Pressable>
-              </View>
+            <View style={styles.delivery}>
+              <Text style={styles.deliveryText}>Delivery time :</Text>
+              <Text style={styles.deliveryTime}> 20 Minutes</Text>
             </View>
+            <View style={styles.quantityContainer}>
+              <Pressable
+                style={styles.quantityMinus}
+                onPress={() => {
+                  if (quantity > 1) {
+                    setQuantity(quantity - 1);
+                  }
+                }}
+              >
+                <Text style={styles.quantityText}>-</Text>
+              </Pressable>
+              <Text style={styles.quantityText}>{quantity}</Text>
+              <Pressable
+                style={styles.quantityPlus}
+                onPress={() => {
+                  if (quantity < 9) {
+                    setQuantity(quantity + 1);
+                  }
+                }}
+              >
+                <Text style={styles.quantityText}>+</Text>
+              </Pressable>
+            </View>
+            {/* <View style={styles.controls}> */}
+            <View style={styles.control}>
+              <Pressable android_ripple={{ color: Colors.rippleDark }}>
+                <Text style={styles.controlText}>Add for {price}</Text>
+              </Pressable>
+            </View>
+            {/* </View> */}
           </View>
         </View>
       </ScrollView>
@@ -65,10 +81,7 @@ const styles = StyleSheet.create({
   rootContainer: {
     justifyContent: "center",
     alignItems: "center",
-  },
-  contentContainer: {
-    width: "80%",
-    marginVertical: 30,
+    width: "100%",
   },
   imageContainer: {
     shadowColor: "#000",
@@ -77,44 +90,60 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     elevation: 20,
     width: "100%",
+    marginTop: 5,
   },
   image: {
     width: "100%",
     height: 250,
   },
+  contentContainer: {
+    width: "90%",
+    marginVertical: 10,
+  },
   dishName: {
     fontSize: 23,
     fontWeight: "bold",
-    marginTop: 20,
-    backgroundColor: Colors.primaryYellow,
-    textAlign: "center",
+    marginTop: 0,
+    // backgroundColor: Colors.primaryYellow,
+    // textAlign: "center",
   },
   description: {
-    fontSize: 16,
-    marginTop: 20,
+    fontSize: 14,
+    marginTop: 5,
   },
-  price: {
+  delivery: {
+    marginTop: 20,
+    flexDirection: "row",
+  },
+  deliveryText: {
+    fontSize: 16,
+  },
+  deliveryTime: {
     fontSize: 16,
     fontWeight: "bold",
-    marginTop: 20,
-  },
-  controls: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "center",
-    marginTop: 40,
   },
   control: {
-    marginHorizontal: 10,
-    backgroundColor: Colors.primaryGray,
+    borderRadius: 15,
+    width: "100%",
+    backgroundColor: Colors.primaryYellow,
+    marginTop: 20,
+    overflow: "hidden",
   },
   controlText: {
-    color: Colors.primaryYellow,
-    width: 100,
-    height: 35,
+    color: Colors.primaryBlack,
     textAlign: "center",
-    lineHeight: 35,
-    fontSize: 14,
+    lineHeight: 45,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  quantityContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  quantityMinus: { marginRight: 30 },
+  quantityPlus: { marginLeft: 30 },
+  quantityText: {
+    fontSize: 30,
     fontWeight: "bold",
   },
 });
